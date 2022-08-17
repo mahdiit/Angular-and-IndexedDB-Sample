df9b3b13c56ef0dcb2054ae1c4cf3e8880517eb1
m.yousefi <m.yousefi@titan-diesel.com>
2022-08-17 16:14:11 +0430

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Dexie } from "dexie"
+import { DbEntity, Repository } from "sample-repository-pattern"
+
+class PersonInfo extends DbEntity {
+  public Id?: number;
+  public FirstName: string;
+  public LastName: string;
+  public MobileNumber?: string;
+  public CreatedDate: Date
+
+  constructor(firstName: string, lastName: string) {
+    super();
+
+    this.FirstName = firstName;
+    this.LastName = lastName;
+    this.CreatedDate = new Date();
+  }
+}
+
+class PersonListDb extends Dexie {
+  Person: Repository<PersonInfo, number>;
+
+  constructor() {
+    super("PersonListDb");
+
+    this.version(2).stores({
+      person: "++Id,FirstName,LastName,MobileNumber,CreatedDate"
+    });
+
+    this.Person = new Repository<PersonInfo, number>(this, "person");
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'FirstAngularApp';
+export class AppComponent implements OnInit {  
+  Db?: PersonListDb;
+  public dataSource = new MatTableDataSource<PersonInfo>();
+  public displayedColumns = ['Id', 'FirstName', 'LastName', 'MobileNumber', 'CreatedDate' ];
+
+  constructor() {
+    (async () => {
+      console.log("constructor");
+    })();
+  }
+
+  ngOnInit(): void {
+    (async () => {
+      console.log("ngOnInit");      
+      this.Db = new PersonListDb();
+      //await this.Db.Person.Add(new PersonInfo("Alireza", "S-" + (new Date()).getTime().toString()));
+      //await this.Db.Person.Add(new PersonInfo("Saeed", "TY-" + (new Date()).getMinutes().toString()));
+      this.dataSource.data = await this.Db.Person.GetAll();
+      //console.table(this.Source.localdata);
+    })();
+  }
+
+  ngAfterViewInit() {
+    console.log("ngAfterViewInit");    
+  }
+
+  title = 'First Angular App';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppMaterialModule } from "./app.material-module";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule,   
+    AppMaterialModule, 
+    AppRoutingModule, 
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
