fd6c60624103fa74f9b46c4c1be720d717137839
m.yousefi <m.yousefi@titan-diesel.com>
2022-08-21 09:58:34 +0430

    css design

--- src/app/app.material-module.ts
+++ src/app/app.material-module.ts
@@ -5,7 +5,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSortModule } from '@angular/material/sort';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatDivider, MatDividerModule } from '@angular/material/divider'
 @NgModule({
     // since we're exporting these modules, add them to export
     exports: [
@@ -14,7 +14,8 @@ import { MatButtonModule } from '@angular/material/button';
         MatProgressSpinnerModule,
         MatInputModule,
         MatPaginatorModule,
-        MatButtonModule
+        MatButtonModule,
+        MatDividerModule
     ]
 })
 export class AppMaterialModule { }
